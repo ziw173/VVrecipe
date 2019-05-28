30194d1ced425d4563643ef772fa2fe25598e180
aaerok <bbk001@ucsd.edu>
2019-05-28 16:42:54 -0700

    modified profile onclick

--- VegHub/routes/profile.js
+++ VegHub/routes/profile.js
@@ -1,26 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
+const User = require('../models/user');
 
 router.get('/', (req, res) => {
     if (req.session.user) {
         const user = req.session.user;
-        Post.find({ _id: { $in : user.saved}}, (err, s) => {
-            if (err) return next(err);
-            Post.find({ _id : { $in : user.recipes}}, (err, r) => {
-                res.render('profile', {
-                    user: user.username,
-                    first: user.firstName,
-                    last: user.lastName,
-                    image: user.image,
-                    bio: user.bio,
-                    recipes: r,
-                    saved: s,
-                    likes: user.likes
+        User.findById(user._id, (err, u) => {
+            Post.find({ _id: { $in : u.saved}}, (err, s) => {
+                if (err) return next(err);
+                Post.find({ _id : { $in : user.recipes}}, (err, r) => {
+                    res.render('profile', {
+                        user: user.username,
+                        recipes: r,
+                        saved: s,
+                    });
                 });
             });
-        });
-        
+        });  
     } else {
         res.render('profile');
     }
